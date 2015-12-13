c4f9c7bae4ab345e24b511848a1f9cc328d1adc1
masquezorra <masquezorra@gmail.com>
2015-12-13 17:02:39 +0000

    bla
    bla

---
+++ App/main.js
@@ -0,0 +1,58 @@
+﻿getAuthors = [];
+$.when(getJson("http://192.168.160.36/JSON/getAuthors.aspx", {})).done(function (a1) {
+    var arr = a1;
+    getAuthors = arr;
+});
+
+getTitles = [];
+$.when(getJson("http://192.168.160.36/JSON/getTitles.aspx", {})).done(function (a1) {
+    var arr = a1;
+    getTitles = arr;
+});
+
+function getJson(myurl, mydata) {
+    return $.ajax({
+        type: "GET",
+        url: myurl,
+        async: false,
+        data: mydata,
+        dataType: "jsonp",
+        error: function (xhr, status, err) { }
+    });
+}
+
+
+
+requirejs.config({
+    paths: {
+        'text': '../Scripts/text',
+        'durandal': '../Scripts/durandal',
+        'plugins': '../Scripts/durandal/plugins',
+        'transitions': '../Scripts/durandal/transitions'
+    }
+});
+
+define('jquery', function () { return jQuery; });
+define('knockout', ko);
+
+define(['durandal/system', 'durandal/app', 'durandal/viewLocator'],  function (system, app, viewLocator) {
+    //>>excludeStart("build", true);
+    system.debug(true);
+    //>>excludeEnd("build");
+
+    app.title = 'ITW Starter Kit';
+
+    app.configurePlugins({
+        router: true,
+        dialog: true
+    });
+
+    app.start().then(function() {
+        //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
+        //Look for partial views in a 'views' folder in the root.
+        viewLocator.useConvention();
+
+        //Show the app by setting the root view model for our application with a transition.
+        app.setRoot('viewmodels/shell', 'entrance');
+    });
+});
---
+++ App/viewmodels/welcome.js
@@ -0,0 +1,49 @@
+﻿define(function () {
+    b = getTitles;
+    out = " "
+    used = []
+    for (var i = 0; i < 3; i++) {
+        z = Math.floor((Math.random() * b.length));
+        console.log(z);
+        console.log(used);
+        while (used.indexOf(z) != -1) {
+            console.log("Let's try this again.");
+            z = Math.floor((Math.random() * b.length));
+        }
+        out += '<div class="col-lg-4 col-sm-12 col-md-6" style=""><center><img src="App/m_media/pictures/littlebook.png" alt="..."><p>'
+            + "<b>" + b[z].title + "</b><p>" + "Autor: " + b[z].authors + "<p>"
+            + '<button type="button" class="btn btn-default btn-sm" data-toggle="modal" data-target="#myModal' + z + '">Detalhes</button>'
+            + '<div class="modal fade" id="myModal' + z + '" role="dialog">'
+            + '<div class="modal-dialog">'
+            + '<div class="modal-content">'
+            + '<div class="modal-header">'
+            + '<button type="button" class="close" data-dismiss="modal">&times;</button>'
+        out += '<h4 class="modal-title">About <b>' + b[z].title + '</b></h4></div>'
+            + '<div class="modal-body">'
+            + b[z].notes + '<hr />' 
+            + '<p style="text-align: left">Genre: ' + b[z].type + '<br>'
+            + 'Publication Date: ' + b[z].pubdate + '<br>'
+            + 'Sales: '+b[z].ytd_sales+'</p>'
+            + '</div>'
+            + '<div class="modal-footer">'
+            + '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>'
+            + '</div></div></div></div></div> ';
+        used.push(z);
+    }
+    return {
+        tast :  out,
+        displayName : 'Bemvindo à ITW App!',
+        description : 'TESTE123',
+        features : [
+            'Clean MV* Architecture',
+            'JS & HTML Modularity',
+            'Simple App Lifecycle',
+            'Eventing, Modals, Message Boxes, etc.',
+            'Navigation & Screen State Management',
+            'Consistent Async Programming w/ Promises',
+            'App Bundling and Optimization',
+            'Use any Backend Technology',
+            'Built on top of jQuery, Knockout & RequireJS'
+        ]
+    };
+});
